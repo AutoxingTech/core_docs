9caae638308f7f75284d14c81edde89616f13483
leidongxiang <leidx@autoxing.com>
2026-09-15 21:40:25 +0800

    docs: Add IMU product documentation and specifications

--- sidebars.js
+++ sidebars.js
@@ -40,6 +40,7 @@ const sidebars = {
                 'hardware-specifications/power-box/index',
                 'hardware-specifications/power-box-48v/index',
                 'hardware-specifications/esp-beacon/index',
+                'hardware-specifications/hc32f420-imu/index',
                 'hardware-specifications/bbox-v1/index',
                 'hardware-specifications/wifi-certification/index',
             ],
